1d913557fe33dd450b39eed1f71de14135f9a80f
david <surikatser@mail.ru>
2023-08-25 10:43:19 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# def polin(str):
+#     if str[::-1] == str:
+#         return True
+#     else:                              ''максимум 1 занятие 4 дз''
+#         return False
+# print(polin(input('Введите слово')))
